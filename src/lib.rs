ac4acadefe0148985227a5ba952c4f8886fbb0cb
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-12-21 09:24:44 +0500

    feat: initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -59,19 +59,21 @@ pub fn run(config: Config) -> MyResult<()> {
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
             Ok(file) => {
+                let mut last_num = 0;
                 for (i, line) in file.lines().enumerate() {
-                    if line.is_ok() {
-                        if config.number_lines {
-                            println!("{}\t{}", i + 1, line.unwrap());
-                            return Ok(());
-                        }
+                    let line = line?;
 
-                        if config.number_nonblank_lines {
-                            println!("{} {}", i + 1, line.unwrap());
-                            return Ok(());
+                    if config.number_lines {
+                        println!("{:>6}\t{}", i + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
                         }
-
-                        println!("{}", line.unwrap())
+                    } else {
+                        println!("{}", line)
                     }
                 }
             }
